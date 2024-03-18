d139532a2c136db4bfe315d773b066d95ee0875e
yeabsira-mesfin <yeabsira.mesfin209@gmail.com>
2024-03-18 17:00:07 -0400

    objects

--- JS/main.js
+++ JS/main.js
@@ -12,13 +12,37 @@
 // console.log(`My name is ${name} and I am ${age} years old and I rate my self ${rating} out of 5 for hardworking`)
 // console.log(name.split(', '))
 
-let fruits = ['Banana','Apple','Mango','Strawberries']
-fruits.push('Grapes')
-fruits.unshift('Strawberries')
-fruits.pop()
+// let fruits = ['Banana','Apple','Mango','Strawberries']
+// fruits.push('Grapes')
+// fruits.unshift('Strawberries')
+// fruits.pop()
 
-console.log(Array.isArray(fruits))
-console.log(Array.isArray('grape'))
-console.log(fruits.indexOf('Apple'))
-console.log(fruits)
+// console.log(Array.isArray(fruits))
+// console.log(Array.isArray('grape'))
+// console.log(fruits.indexOf('Apple'))
+// console.log(fruits)
 
+const me = {
+    firstName: 'Yeabsira',
+    lastName: 'Mesfin',
+    age: 30,
+    hobbies: ['Coding','Reading','Football'],
+    address: {
+        street: 'this',
+        city: 'is',
+        state: 'a secret'
+    }
+}
+
+
+
+console.log(me.hobbies[2])
+console.log(` My address: ${me.address.street} ${me.address.city} ${me.address.state}`)
+
+// Destructuring
+
+const {firstName,lastName,address: {city}} = me;
+
+console.log(city)
+
+console.log(firstName)
