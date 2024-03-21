d767d353f5e991729c74f717d6c7a4405372dba9
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-03-21 10:55:58 -0400

    event

--- JS/main.js
+++ JS/main.js
@@ -7,13 +7,19 @@
 // console.log(document.getElementsByClassName('item')) // This gives HTMLCollection.
 // console.log(document.getElementsByTagName('li'))
 
-const ul = document.querySelector('.items');
-// ul.remove()
-// ul.lastElementChild.remove();
-const text = ul.firstElementChild.textContent = 'Hello';
-ul.children[1].textContent = 'Siuu'
-ul.children[2].innerText = 'iuu'
-ul.lastElementChild.innerHTML = '<h1>Holla</h1>'
-
-const btn = document.querySelector('.btn');
-btn.style.background = 'blue'
+// const ul = document.querySelector('.items');
+// // ul.remove()
+// // ul.lastElementChild.remove();
+// const text = ul.firstElementChild.textContent = 'Hello';
+// ul.children[1].textContent = 'Siuu'
+// ul.children[2].innerText = 'iuu'
+// ul.lastElementChild.innerHTML = '<h1>Holla</h1>'
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'blue'
+// btn.addEventListener('click',(e)=>{
+//     e.preventDefault()
+//    console.log('clicked')
+//    console.log(e.target.className)
+//    document.querySelector('.items').classList.add('bg-dark');
+//    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
+// }) 
