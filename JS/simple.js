cf7371bff1427b0920b5cbdb2e08447f7a70d7de
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-03-22 07:52:21 -0400

    golf score

--- JS/simple.js
+++ JS/simple.js
@@ -1,7 +1,7 @@
 /// Nested array (arrays in arrays.)
-let ourArray = ["the universe", 2,"is inifnite",10]
+// let ourArray = ["the universe", 2,"is inifnite",10]
 
-console.log(ourArray[1])
+// console.log(ourArray[1])
 
 /// Nested within nested array
 
@@ -17,21 +17,47 @@ console.log(ourArray[1])
 
 /// Push: elements at the end of an array.
 
-ourArray.push('Dog')
-ourArray.push([2,5])
-
-console.log(ourArray)
+// ourArray.push('Dog')
+// ourArray.push([2,5])
+// console.log(ourArray)
 
 /// pop removes the last element.
 
-ourArray.pop()
-console.log(ourArray)
+// ourArray.pop()
+// console.log(ourArray)
 
 /// shift removes the first element.
 
-ourArray.shift();
-console.log(ourArray)
+// ourArray.shift();
+// console.log(ourArray)
 
 /// unshift: adds elements to the begining of an array
 
-console.log(ourArray.unshift('Happy'))
+// console.log(ourArray.unshift('Happy'))
+
+/// Golf code.
+
+function golfScore(par, stroke){
+    if(stroke == 1){
+        return 'Hole-in-one';
+    }
+    else if(stroke <=par -2){
+        return 'Eagle';
+    }
+    else if(stroke == par -1){
+        return 'Birdie';
+    }
+    else if(stroke == par){
+        return 'par';
+    }
+    else if(stroke == par + 1){
+        return 'Bogey';
+    }
+    else if(stroke == par + 2){
+        return 'Double Bogey';
+    }
+    else if(stroke >= par +3){
+        return 'Go home.'
+    }
+}
+console.log(golfScore(2,4))
