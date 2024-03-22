d89a39e1d24bc629e5e9a784f6c64af602486051
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-03-22 07:23:41 -0400

    nested within nested

--- JS/simple.js
+++ JS/simple.js
@@ -2,3 +2,13 @@
 let ourArray = [["the universe", 2],["is inifnite",10]]
 
 console.log(ourArray[1][1])
+
+/// Nested within nested array
+
+let superArray = [[2,3],[5,4],[[6,8],[['Hey','Soul'],['why','you?']]]]
+console.log(superArray[0][1]);
+console.log(superArray[2][0][1]);
+console.log(superArray[2][1][0][0]);
+console.log(superArray[2][1][0][1]);
+console.log(superArray[2][1][1][0]);
+console.log(superArray[2][1][1][1]);
