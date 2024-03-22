b7708a94f96bfb1f33e1035764f16377746b147a
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-03-22 07:16:42 -0400

    Nested array

--- JS/main.js
+++ JS/main.js
@@ -22,4 +22,31 @@
 //    console.log(e.target.className)
 //    document.querySelector('.items').classList.add('bg-dark');
 //    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
-// }) 
+// })
+
+const myForm = document.querySelector('#my-form');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const msg = document.querySelector('.msg')
+const userList = document.querySelector('#users')
+
+myForm.addEventListener('submit', onSbumit);
+
+function onSbumit(e){
+    e.preventDefault();
+    if(name.value === '' || email.value === ''){
+        msg.classList.add('error')
+        msg.innerHTML = 'Please enter both fields';
+
+        setTimeout(() => msg.remove(),3300)
+    }
+    else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${name.value} \n ${email.value}`));
+        userList.appendChild(li);
+
+
+    name.value = '';
+    email.value = '';
+    }
+}
---
+++ JS/simple.js
@@ -0,0 +1,4 @@
+/// Nested array (arrays in arrays.)
+let ourArray = [["the universe", 2],["is inifnite",10]]
+
+console.log(ourArray[1][1])
