2f8f07553293b6d1df29bc228967839c9341671d
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-04-03 06:17:52 -0400

    Error Handling

--- APIs/main.js
+++ APIs/main.js
@@ -78,7 +78,17 @@ function getTodos() {
   
   // ERROR HANDLING
   function errorHandling() {
-    console.log('Error Handling');
+    axios.get('https://jsonplaceholder.typicode.com/todoss')
+    .then(res => showOutput(res))
+    .catch(err => {
+      // Server responsed with a status other than 200
+      
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+      alert('Page not found.')
+    });
+
   }
   
   // CANCEL TOKEN
