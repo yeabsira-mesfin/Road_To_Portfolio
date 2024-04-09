afdb0097f2f9fd8f132c3d247c522ea04c682c81
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-04-09 06:03:28 -0400

    async handler

--- MERN/backend/Controllers/goalController.js
+++ MERN/backend/Controllers/goalController.js
@@ -1,35 +1,36 @@
+const asyncHandler = require('express-async-handler')
 // @desc Get goals
 // @route GET /apai/goals
 // @access Private
 
-const getGoals = (req,res) => {
+const getGoals =asyncHandler( async (req,res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('Please add a text field')
     }
     res.status(200).json({Message: 'Get goals'})
-}
+})
 // @desc POST goals
 // @route POST /apai/goals
 // @access Private
 
-const postGoal = (req,res) => {
+const postGoal = asyncHandler (async (req,res) => {
     res.status(200).json({Message: 'Set goals'})
-}
+})
 // @desc Update goals
 // @route PUT /apai/goals/:id
 // @access Private
 
-const updateGoal = (req,res) => {
+const updateGoal = asyncHandler ( async (req,res) => {
     res.status(200).json({message: `Update goals ${req.params.id}`,name:'Yeabsira'});
-}
+})
 // @desc Update goals
 // @route PUT /apai/goals/:id
 // @access Private
 
-const deleteGoal = (req,res) => {
+const deleteGoal = asyncHandler ( async (req,res) => {
     res.status(200).json({message: `Delte goals ${req.params.id}`,name:'Yeabsira'});
-}
+})
 
 module.exports = {
     getGoals,
