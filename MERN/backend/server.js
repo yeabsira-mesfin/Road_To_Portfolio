e674cee4fe51858b6e710eb36d801097e9e44c35
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-04-10 08:14:54 -0400

    Mongo DB

--- MERN/backend/server.js
+++ MERN/backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const colors = require('colors')
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
-const {errorHandler} = require('./Middleware/errorMidleware')
+const {errorHandler} = require('./Middleware/errorMidleware');
+const connectDB = require('./Config/db')
 const app = express();
+
+connectDB();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
