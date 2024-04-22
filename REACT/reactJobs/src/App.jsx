36a60c2dc0ff76b7a4dcdf42705c9ba0691c6502
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-04-22 06:44:56 -0400

    JSX

--- REACT/reactJobs/src/App.jsx
+++ REACT/reactJobs/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
 const App = () => {
+  const name = 'Yeab'
+  const x = 9;
+  const y = 20;
+  const names = ['Yeab','Yisu','John']
+  const loggedIn = false
+  const styles = {
+    color: 'blue',
+    fontSize: '33px'
+  }
+  
+ 
   return (
-    <div>App</div>
-  )
+    <>
+      <div className='text-5xl'>App</div>
+      <p style={styles}>Hello {name}</p>
+      <p>The sum of {x} and {y} is {x + y}</p>
+      <ul>
+        {names.map((name,index)=>(
+          <li key={index}>{name}</li>
+        ))}
+       
+      </ul>
+      {loggedIn ?  <h1>Hello Member</h1>:  <h1> Hello Guest</h1>}
+    </>
+  );
 }
 
 export default App
