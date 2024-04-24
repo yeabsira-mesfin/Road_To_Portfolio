0ac6f47036d43eeba3946f690fee0eaf2dc7a8a4
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-04-24 06:19:32 -0400

    Hero

--- REACT/reactJobs/src/App.jsx
+++ REACT/reactJobs/src/App.jsx
@@ -1,29 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 const App = () => {
   return (
     <>
     <NavBar/>
+   <Hero/>
    
-
-    {/* <!-- Hero --> */}
-    <section className="bg-indigo-700 py-20 mb-4">
-      <div
-        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-      >
-        <div className="text-center">
-          <h1
-            className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
-          >
-            Become a React Dev
-          </h1>
-          <p className="my-4 text-xl text-white">
-            Find the React job that fits your skills and needs
-          </p>
-        </div>
-      </div>
-    </section>
-
     {/* <!-- Developers and Employers --> */}
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
