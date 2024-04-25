4e33fcc87393169b0106f7cc49f7af8b2266d578
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-04-25 06:39:11 -0400

    fap marker

--- REACT/reactJobs/src/components/BrowseJob.jsx
+++ REACT/reactJobs/src/components/BrowseJob.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
+import {FaMapMarker} from 'react-icons/fa'
+
 export const BrowseJob = ({job}) => {
   const [showFullDescription,setShowFullDescription] = useState(false);
   let description = job.description;
@@ -28,7 +30,7 @@ export const BrowseJob = ({job}) => {
 
       <div className="flex flex-col lg:flex-row justify-between mb-4">
         <div className="text-orange-700 mb-3">
-          <i className="fa-solid fa-location-dot text-lg"></i>
+          <FaMapMarker className='inline text-lg- mb-1'/>
          {job.location}
         </div>
         <a
