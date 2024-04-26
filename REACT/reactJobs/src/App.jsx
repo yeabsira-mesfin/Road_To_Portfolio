adb0af6ada2e4916162b0102803cee1e777dece3
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-04-26 06:48:19 -0400

    Link Tag

--- REACT/reactJobs/src/App.jsx
+++ REACT/reactJobs/src/App.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import HomPage from './Pages/HomPage';
 import MainLayout from './Layouts/MainLayout';
+import JobsPage from './Pages/JobsPage';
+import AddJob from './Pages/AddJob';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout/>}>
-      <Route index element={<HomPage/>}/>
+      <Route path='/' element={<HomPage/>}/>
+      <Route path='/jobs' element={<JobsPage/>}/>
+      <Route path='/addjobs' element={<AddJob/>}/>
     </Route>
 )
 );
