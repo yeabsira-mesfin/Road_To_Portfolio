d3a1db89de9c73ea4fd15f4d269f8be5adbb6e55
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-04-26 06:03:10 -0400

    Router

--- REACT/reactJobs/src/App.jsx
+++ REACT/reactJobs/src/App.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import EmployeeDevelopers from './components/EmployeeDevelopers'
 import BrowseJobs from './components/BrowseJobs'
 import ViewMore from './components/ViewMore'
 const App = () => {
-  return (
-    <>
-    <NavBar/>
-   <Hero />
-   <EmployeeDevelopers/>
-   <BrowseJobs/>
-   <ViewMore/>
-    </>
-  )
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(<Route index element={<h1>My app</h1>}/>)
+  );
+  return <RouterProvider router={router}/>
 }
 
 export default App
