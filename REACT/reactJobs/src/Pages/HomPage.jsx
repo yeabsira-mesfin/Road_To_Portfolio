7f3475ae2669a75dbd5f505872728847de4e2ee3
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-04-26 06:37:05 -0400

    Main layout and home page

---
+++ REACT/reactJobs/src/Pages/HomPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import EmployeeDevelopers from '../components/EmployeeDevelopers'
+import  BrowseJobs  from '../components/BrowseJobs'
+import ViewMore from '../components/ViewMore'
+const HomPage = () => {
+  return (
+    <>
+    <Hero></Hero>
+    <EmployeeDevelopers></EmployeeDevelopers>
+    <BrowseJobs></BrowseJobs>
+    <ViewMore></ViewMore>
+    </>
+  )
+}
+
+export default HomPage
